7df9be41090367e9ac2def698ebb436286a6aff4
Reg Martin <3178688+rgmrtn@users.noreply.github.com>
2020-12-14 21:53:23 -0500

    TODO: logging

--- downhole/survey.py
+++ downhole/survey.py
@@ -1,6 +1,9 @@
 from math import acos, cos, sin, radians, degrees, tan, copysign, sqrt
 from .exceptions import *
+import logging
 
+logging.basicConfig(filename='app.log', filemode='w', format='%(name)s - %(levelname)s - %(message)s')
+logging.warning('This will get logged to a file NEW')
 
 class Survey():
     """A set of downhole surveys"""
